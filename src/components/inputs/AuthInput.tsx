3c396f9ee377c2cabcc9eadde8ea9bfcef933195
Denys Markevych <denysjse@gmail.com>
2024-08-08 17:17:53 +0300

    Brought out used colors to variable and implement them to styles

--- src/components/inputs/AuthInput.tsx
+++ src/components/inputs/AuthInput.tsx
@@ -25,7 +25,7 @@ function AuthInput({
 		<div className='flex flex-col gap-1'>
 			<label
 				htmlFor={`form-${title.toLowerCase()}`}
-				className='text-sm text-stone-300'
+				className='text-sm text-light-gray'
 			>
 				{inputTitle}
 			</label>
@@ -37,14 +37,15 @@ function AuthInput({
 				})}
 				placeholder={inputTitle}
 				className={cn(
-					'text-white outline-none px-3 py-2 rounded-xl border border-stone-600 bg-stone-800 placeholder:text-stone-300 text-lg focus:border-lime-400',
+					'text-white outline-none px-3 py-2 rounded-xl border border-dark-mode-border bg-dark-gray placeholder:text-light-gray text-lg',
 					{
-						'border-red-500 focus:border-red-500': !!errorMessage
+						'border-dark-mode-border focus:border-lime': !errorMessage,
+						'border-error-red focus:border-error-red': !!errorMessage
 					}
 				)}
 			/>
 			{errorMessage && (
-				<p className='text-red-500 font-semibold text-sm'>{errorMessage}</p>
+				<p className='text-error-red font-semibold text-sm'>{errorMessage}</p>
 			)}
 		</div>
 	)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,53 @@
 /** @type {import('tailwindcss').Config} */
+const colors = {
+	'error-red': '#ef4444',
+	lime: '#a3e635',
+	'light-lime': '#d9f99d',
+	'button-hover': '#e7e5e4',
+	// dark theme colors:
+	'bg-dark-mode': '#1c1917',
+	white: '#ffffff',
+	black: '#000000',
+	'dark-gray': '#292524',
+	gray: '#44403c',
+	'light-gray': '#d6d3d1',
+	'dark-mode-border': '#57534e'
+	// light theme colors:
+}
+
+// const fontSize = {
+// 	xs: '0.82rem',
+// 	sm: '0.98rem',
+// 	base: '1.15rem',
+// 	lg: '1.22rem',
+// 	xl: '1.36rem',
+// 	'1.5xl': '1.5rem',
+// 	'2xl': '1.725rem',
+// 	'3xl': '2.155rem',
+// 	'4xl': '2.58rem',
+// 	'5xl': '3.45rem',
+// 	'6xl': '4.3rem',
+// 	'7xl': '5.17rem',
+// 	'8xl': '6.9rem',
+// 	'9xl': '9.2rem'
+// }
+//
+// const screens = {
+// 	sm: '640px',
+// 	md: '768px',
+// 	lg: '1024px',
+// 	xl: '1280px',
+// 	'2xl': '1536px',
+// 	'3xl': '1850px',
+// 	'4xl': '2120px'
+// }
+
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
+		colors,
+		// fontSize,
+		// screens,
 		extend: {
 			keyframes: {
 				blink: {
